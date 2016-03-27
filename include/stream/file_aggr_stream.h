84ee8d148ecaca21a2471d5061924de53cd6ff27
balsulami <bander@alsulamy.com>
2016-03-27 14:17:46 -0400

    Project Structure: - Add compression file stream to the output.

--- include/stream/file_aggr_stream.h
+++ include/stream/file_aggr_stream.h
@@ -7,11 +7,12 @@
 #include<vector>
 #include<list>
 #include<unordered_map>
+#include "utils/gzstream.h"
 template<class T>
 class file_aggr_stream : public sensor_stream<T>{
 public:
-	file_aggr_stream(const char * name,int size=1024) : _file(name),_size(size){
-
+	file_aggr_stream(const char * name,int size=1024) : _file_name(name),_size(size),_file_index(10) {
+		_file.open((_file_name + "_" + std::to_string(_file_index++) + ".gz").c_str());
 	}
 
 	virtual void dispatch(std::vector<T> & records){
@@ -42,14 +43,22 @@ public:
 			_file<<"\n";
 		}
 		traces.clear();
+		// close this file and open new file
+		if (_file_index % 10 == 0) {
+			_file.flush();
+			_file.close();
+			_file.open((_file_name + "_" + std::to_string(_file_index++) + ".gz").c_str());
+		}
 	}
 
-	~file_aggr_stream(){
+	~file_aggr_stream() {
 		_file.close();
 	}
 
 private:
-	std::ofstream _file;
+	ogzstream _file;
+	std::string _file_name;
 	std::unordered_map<pid_t,std::vector<syscall_t>> traces;
 	int _size ;
+	int _file_index;
 };
